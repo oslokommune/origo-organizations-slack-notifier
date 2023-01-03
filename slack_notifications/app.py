ecdc2e4f673570bd96bd2aef7b9758be2f0f5d5e
Karl Mathias Moberg <km@kmoberg.no>
2023-01-03 10:10:52 +0100

    ✅ Add black formatting and github action

--- slack_notifications/app.py
+++ slack_notifications/app.py
@@ -12,21 +12,21 @@ import requests
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
+SLACK_WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
 
 
 def lambda_handler(event, context):
     # Parse the SNS message
-    logger.info('Starting lambda_handler')
-    logger.info('Event: {}'.format(event))
+    logger.info("Starting lambda_handler")
+    logger.info("Event: {}".format(event))
 
     try:
-        event_bridge_message = event['Records'][0]
+        event_bridge_message = event["Records"][0]
     except KeyError:
-        logger.error('Event does not contain Records')
+        logger.error("Event does not contain Records")
         event_bridge_message = event
 
-    logger.info('Event Bridge Message: {}'.format(event_bridge_message))
+    logger.info("Event Bridge Message: {}".format(event_bridge_message))
 
     message_json = event_bridge_message
 
@@ -49,82 +49,45 @@ def lambda_handler(event, context):
                 "text": {
                     "type": "plain_text",
                     "text": "⚠️  AWS Root User Login",
-                    "emoji": use_emoji
-                }
+                    "emoji": use_emoji,
+                },
             },
             {
                 "type": "section",
                 "text": {
                     "type": "plain_text",
-                    "text": "A login to an Origo AWS account using a root user was detected. Details:"
-                }
+                    "text": "A login to an Origo AWS account using a root user was detected. Details:",
+                },
             },
+            {"type": "divider"},
             {
-                "type": "divider"
+                "type": "context",
+                "elements": [{"type": "mrkdwn", "text": f"*Account ID:* {account}"}],
             },
             {
                 "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f'*Account ID:* {account}'
-                    }
-                ]
+                "elements": [{"type": "mrkdwn", "text": f"*Region:* {region}"}],
             },
             {
                 "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f'*Region:* {region}'
-                    }
-                ]
+                "elements": [{"type": "mrkdwn", "text": f"*Source IP:* {source_ip}"}],
             },
             {
                 "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f'*Source IP:* {source_ip}'
-                    }
-                ]
+                "elements": [{"type": "mrkdwn", "text": f"*Event Name:* {event_name}"}],
             },
             {
                 "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f'*Event Name:* {event_name}'
-                    }
-                ]
+                "elements": [{"type": "mrkdwn", "text": f"*Event Time:* {event_time}"}],
             },
             {
                 "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f'*Event Time:* {event_time}'
-                    }
-                ]
+                "elements": [{"type": "mrkdwn", "text": f"*User Agent:* {user_agent}"}],
             },
             {
                 "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f'*User Agent:* {user_agent}'
-                    }
-                ]
+                "elements": [{"type": "mrkdwn", "text": f"*MFA Used:* {mfa_used}"}],
             },
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f'*MFA Used:* {mfa_used}'
-                    }
-                ]
-            }
         ]
     }
 
